fcfd145c97a29368c0c2d88e2b687a0461e93e59
eethanburns <104241570+eethanburns@users.noreply.github.com>
2025-02-26 12:12:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 const audioContext = new window.AudioContext();
-
 const oscillator = audioContext.createOscillator();
+oscillator.type = "sine";
+oscillator.frequency.value = 440;
+let myGain = audioContext.createGain();
+myGain.gain.value = 0.5;
 
 const updateFrequency = function (f) {
   oscillator.frequency.value = f;
 };
-
-oscillator.type = "sine";
-
-let myGain = audioContext.createGain();
-myGain.gain.value = 0.5;
 
 oscillator.connect(myGain);
 myGain.connect(audioContext.destination);
