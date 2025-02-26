3fd05ae1cbac61328bbe8fbc4e86ea66de18722f
eethanburns <104241570+eethanburns@users.noreply.github.com>
2025-02-26 12:47:33 -0500

    final

--- script.js
+++ script.js
@@ -1,21 +1,47 @@
-const audioContext = new window.AudioContext();
-const oscillator = audioContext.createOscillator();
-oscillator.type = "sine";
-oscillator.frequency.value = 440;
+let startButton = document.getElementById("startButton");
+let stopButton = document.getElementById("stopButton");
+let frequencySlider = document.getElementById("frequencySlider");
+let volumeSlider = document.getElementById("volumeSlider");
+
+const audioContext = new AudioContext();
+let oscillator = null;
 let myGain = audioContext.createGain();
-myGain.gain.value = 0.5;
-
-const updateFrequency = function (f) {
-  oscillator.frequency.value = f;
-};
-
-oscillator.connect(myGain);
+myGain.gain.value = volumeSlider.value;
 myGain.connect(audioContext.destination);
 
 const startAudio = function () {
+  if (!oscillator) {
+    oscillator = audioContext.createOscillator();
+    oscillator.type = "sine";
+    oscillator.frequency.value = frequencySlider.value;
+    oscillator.connect(myGain);
+    oscillator.start();
+  }
   audioContext.resume();
-  oscillator.start();
 };
 
-let theButton = document.getElementById("startButton");
-theButton.addEventListener("click", startAudio);
+const stopAudio = function () {
+  audioContext.suspend();
+  if (oscillator) {
+    oscillator.stop();
+    oscillator = null;
+  }
+};
+
+const updateFrequency = function (f) {
+  oscillator.frequency.value = f;
+  console.log("Frequency: " + f);
+};
+
+const updateVolume = function (v) {
+  myGain.gain.value = v;
+  console.log("Volume: " + v);
+};
+
+startButton.addEventListener("click", startAudio);
+stopButton.addEventListener("click", stopAudio);
+
+frequencySlider.addEventListener("input", () =>
+  updateFrequency(frequencySlider.value)
+);
+volumeSlider.addEventListener("input", () => updateVolume(volumeSlider.value));
