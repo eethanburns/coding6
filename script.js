108f1d65af80ebbc4a2958c2b3e8913d11aa4cdb
eethanburns <104241570+eethanburns@users.noreply.github.com>
2025-02-26 12:51:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
+//Define all web elements
 let startButton = document.getElementById("startButton");
 let stopButton = document.getElementById("stopButton");
 let frequencySlider = document.getElementById("frequencySlider");
 let volumeSlider = document.getElementById("volumeSlider");
 
+//create audio context and gain node
 const audioContext = new AudioContext();
 let oscillator = null;
 let myGain = audioContext.createGain();
 myGain.gain.value = volumeSlider.value;
 myGain.connect(audioContext.destination);
 
+//function to create new oscillator, connect gain node, and resume audio context
 const startAudio = function () {
   if (!oscillator) {
     oscillator = audioContext.createOscillator();
@@ -20,6 +23,7 @@ const startAudio = function () {
   audioContext.resume();
 };
 
+//function to stop audio and suspend audio context
 const stopAudio = function () {
   audioContext.suspend();
   if (oscillator) {
@@ -28,19 +32,23 @@ const stopAudio = function () {
   }
 };
 
+//function to update frequency
 const updateFrequency = function (f) {
   oscillator.frequency.value = f;
   console.log("Frequency: " + f);
 };
 
+//function to update volume
 const updateVolume = function (v) {
   myGain.gain.value = v;
   console.log("Volume: " + v);
 };
 
+//event listeners for buttons
 startButton.addEventListener("click", startAudio);
 stopButton.addEventListener("click", stopAudio);
 
+//event listeners for sliders
 frequencySlider.addEventListener("input", () =>
   updateFrequency(frequencySlider.value)
 );
